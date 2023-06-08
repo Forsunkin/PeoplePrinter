d478fb2250b1987d8ddba31a7618c632001dd473
Forsunkin <guazah@gmail.com>
2023-06-08 14:26:48 +0300

    complited get_toner, get_counter methods

--- get_data_kyocera.py
+++ get_data_kyocera.py
@@ -1,21 +1,31 @@
 import requests
-url = 'http://192.168.1.38/js/jssrc/model/dvcinfo/dvccounter/DvcInfo_Counter_PrnCounter.model.htm'
 import re
 
-# def get_data(url):
-#     r = requests.get(url)
-#     print(r.text)
+first_ip = '192.168.1.38'
 
-# get_data(url)
+url_toner = f'http://{first_ip}/js/jssrc/model/startwlm/Hme_Toner.model.htm'
+url_couner = f'http://{first_ip}/js/jssrc/model/dvcinfo/dvccounter/DvcInfo_Counter_PrnCounter.model.htm'
 
-f = open("exemple_kyocera.txt", "r")
-data = f.read()
 
-def parse_data(obj):
-    printer_total = re.findall(r".*printertotal = \('(\d*)'\)", obj)
-    copy_total = re.findall(r".*copytotal = \('(\d*)'\)", obj)
-    result = int(printer_total[0]) + int(copy_total[0])
+
+
+
+# Получить количество отпечатаных листов
+def get_counter(ip):
+    rst = requests.get(url_couner, ip)
+    printer_total = re.findall(r".*printertotal = \('(\d*)'\)", rst.text)           # получить отпечатанные листы
+    copy_total = re.findall(r".*copytotal = \('(\d*)'\)", rst.text)                 # получить сканы
+    result = int(printer_total[0]) + int(copy_total[0])                             # получить тотал
+    return result
+
+# Получить отсток тоннера
+def get_toner(ip):
+    rst = requests.get(url_toner, ip)
+    toner = re.findall(r".*parseInt\('(\d*)',10\)\)", rst.text)                     # получить остаток тонера
+    result = toner [0]
     return result
 
 
-print(parse_data(data))
+print(get_counter('192.168.1.38'))
+print(get_toner('192.168.1.38'))
+
