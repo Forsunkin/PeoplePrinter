3828a9a8ec19994045df2b019a9b9162fc42abb6
Forsunkin <guazah@gmail.com>
2023-06-20 18:04:48 +0300

    creating class

--- test_get_data_hp.py
+++ test_get_data_hp.py
@@ -1,17 +1,32 @@
-import requests
+import re, requests
 from bs4 import BeautifulSoup
-import re
 
-url = "http://192.168.1.99/info_suppliesStatus.html?tab=Home&menu=SupplyStatus" # SPA
 
-def get_data(url):
-    source_code = requests.get(url).text
-    bs_code = BeautifulSoup(source_code, "html.parser")
-    data_numbers = bs_code.find_all(class_='itemSpsFont')
-    data_toner = bs_code.find(class_='SupplyName width35 alignRight')
+urlspa = "http://192.168.1.99/info_suppliesStatus.html?tab=Home&menu=SupplyStatus" # SPA
 
-    
-    toner_lvl = re.findall(r"(\d+)", data_toner.text)
-    return toner_lvl
+class HPData:
+    def __init__(self, ip):
+        self.get_da1ta = '123'
 
-print(get_data(url))
+    def get_data(ip):
+        url = f"http://{ip}/info_suppliesStatus.html?tab=Home&menu=SupplyStatus"
+        source_code = requests.get(url).text
+        bs_code = BeautifulSoup(source_code, "html.parser")
+        data_numbers = bs_code.find_all(class_='itemSpsFont')
+        data_toner = bs_code.find(class_='SupplyName width35 alignRight')
+
+
+        toner_lvl = re.findall(r"(\d+)", data_toner.text)           #уровень тонера
+        return data_numbers
+
+    def get_config_hp(ip):
+        url = f"http://{ip}/info_configuration.html?tab=Home&menu=DevConfig"
+        source_code = requests.get(url).text
+        bs_code = BeautifulSoup(source_code, "html.parser")
+        print(bs_code)
+
+
+ip = '192.168.1.99'
+
+r = HPData(ip)
+r.get_data()
