2b631f6f2e2fadeecafecd14e72ed39e319bd446
Forsunkin <guazah@gmail.com>
2023-06-29 12:09:10 +0300

    completed getting data HP and Kyocera

--- base.py
+++ base.py
@@ -2,27 +2,23 @@ import sqlite3, re, requests, datetime
 from bs4 import BeautifulSoup
 
 
-
-
-
 sqlite_connection = sqlite3.connect('people_printers.db')
 print("Подключен к SQLite")
+
 
 def create_table():
     cursor = sqlite_connection.cursor()
     sqlite_create_table = f'''CREATE TABLE if not exists data_printers (
-    	ip TEXT,
-    	mac TEXT,
-    	hostname TEXT,
-    	firma TEXT,
-    	kartrige TEXT,
-    	object TEXT,
-    	locate TEXT,
-    	PRIMARY KEY(mac)
+                        ip TEXT,
+                        mac TEXT,
+                        hostname TEXT,
+                        firma TEXT,
+                        cartridge TEXT,
+                        object TEXT,
+                        locate TEXT,
+                        PRIMARY KEY(mac)
     );'''
 
     cursor.execute(sqlite_create_table)
     sqlite_connection.commit()
     sqlite_connection.close()
-
-def get
