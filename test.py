4534eae0879ff89802ae0c1559b45f48fdbb6d79
Forsunkin <guazah@gmail.com>
2023-06-29 16:38:10 +0300

    completed getting data HP and Kyocera

--- test.py
+++ test.py
@@ -1,10 +1,59 @@
 import requests
-from get_data_kyocera import get_prints_toner_kyocera
-from get_data_hp import get_prints_toner_hp
-from kostili import kostil_base_get_list_ip as list_ip
+import re
+from bs4 import BeautifulSoup
+from kostili import kostil_base_get_list_ip as list_ip      # Временный костыль ip_list - список Ip адрессов
 
-# Временный костыль ip_list - список Ip адрессов
 
+def get_prints_toner_kyocera(ip_address):
+    url_counter = f'http://{ip_address}/js/jssrc/model/dvcinfo/dvccounter/DvcInfo_Counter_PrnCounter.model.htm'
+    url_toner = f'http://{ip_address}/js/jssrc/model/startwlm/Hme_Toner.model.htm'
+
+    headers = {'Cookie': 'rtl=0; css=0',
+                     'Referer': f"http://{ip_address}/startwlm/Start_Wlm.htm"}
+
+    page_code_config = requests.get(url_counter, headers=headers)
+    page_code_toner = requests.get(url_toner, headers=headers)
+
+    # Получение данных ко-ва оттисков
+    try:
+        printed_total = re.findall(r".*printertotal = \('(\d*)'\)", page_code_config.text)# получить отпечатанные листы
+        copy_total = re.findall(r".*copytotal = \('(\d*)'\)", page_code_config.text)      # получить сканы
+        prints_count = int(printed_total[0]) + int(copy_total[0])                         # получить cумму
+    except (ValueError, IndexError):
+        prints_count = 'Error'
+
+    # получение уровня тонера
+    try:
+        data_toner = re.findall(r".*parseInt\('(\d*)',10\)\)", page_code_toner.text)      # получить остаток тонера
+        toner_lvl = int(data_toner[0])
+    except (ValueError, IndexError):
+        toner_lvl = 'Error'
+
+    finally:
+        return toner_lvl, prints_count
+
+
+def get_prints_toner_hp(ip_address):
+
+    url_config = f'''http://{ip_address}/info_configuration.html?tab=Home&menu=DevConfig'''      # page with config info
+    url_toner = f'''http://{ip_address}/info_suppliesStatus.html?tab=Home&menu=SupplyStatus'''   # page with toner
+
+    page_code_config = requests.get(url_config).text
+    page_code_toner = requests.get(url_toner).text
+
+    bs_code_config = BeautifulSoup(page_code_config, "html.parser")
+    bs_code_toner = BeautifulSoup(page_code_toner, "html.parser")
+
+    # Получение данных ко-ва оттисков
+    td = bs_code_config.find("td", string='Всего оттисков:')
+    td_parent = td.find_parent('tr')
+    prints_count = td_parent.find(class_='itemFont').text                                       # общее кол-во оттисков
+
+    # Работа с данными по тонеру
+    data_toner = bs_code_toner.find(class_='SupplyName width35 alignRight')
+    toner_lvl = (re.findall(r"(\d+)", data_toner.text)[0])                                        # уровнеь тонера
+
+    return int(toner_lvl), int(prints_count)
 
 
 
@@ -22,12 +71,13 @@ def get_data_printer(ip_address):
     else:
         print(ip_address, 'Неверный ip')
 
+
 def getting_info():
     for ip_address in list_ip():
         try:
             info = get_data_printer(ip_address)
-        except (ValueError, IndexError, requests.exceptions.ConnectTimeout):
-            print(f'{ip_address}- fail get')
+        except (requests.exceptions.ConnectTimeout):
+            print(f'{ip_address}- ConnectTimeout')
 
 
 
