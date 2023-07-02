63a93b27421b03fa3421a80678eaeca42c8fc613
forsunkin <guazah@gmail.com>
2023-07-02 15:44:48 +0300

    mac, ip, locate, td

--- test.py
+++ test.py
@@ -7,12 +7,14 @@ from kostili import kostil_base_get_list_ip as list_ip      # Временный
 def get_prints_toner_kyocera(ip_address):
     url_counter = f'http://{ip_address}/js/jssrc/model/dvcinfo/dvccounter/DvcInfo_Counter_PrnCounter.model.htm'
     url_toner = f'http://{ip_address}/js/jssrc/model/startwlm/Hme_Toner.model.htm'
+    url_info = f'http://{ip_address}/js/jssrc/model/dvcinfo/dvcconfig/DvcConfig_Config.model.htm'
 
     headers = {'Cookie': 'rtl=0; css=0',
                      'Referer': f"http://{ip_address}/startwlm/Start_Wlm.htm"}
 
     page_code_config = requests.get(url_counter, headers=headers)
     page_code_toner = requests.get(url_toner, headers=headers)
+    page_code_info = requests.get(url_info, headers=headers)
 
     # Получение данных ко-ва оттисков
     try:
@@ -22,15 +24,31 @@ def get_prints_toner_kyocera(ip_address):
     except (ValueError, IndexError):
         prints_count = 'Error'
 
-    # получение уровня тонера
+    # Получение уровня тонера
     try:
         data_toner = re.findall(r".*parseInt\('(\d*)',10\)\)", page_code_toner.text)      # получить остаток тонера
         toner_lvl = int(data_toner[0])
     except (ValueError, IndexError):
         toner_lvl = 'Error'
 
+
+    try:
+        mac_address = re.findall(r".*macAddress = '(.*)'", page_code_info.text)  # получить mac адресс
+        model = re.findall(r".*model = '.* (.*)'", page_code_info.text)  # получть Модель
+        host_name = re.findall(r".*hostName = '(.*)'", page_code_info.text)  # получить HostName
+
+        f = re.findall(r"192.168.(\d*).\d*", ip_address)[0]
+        locale = 'Неизвестно'  # Получить расположение
+        if f == '1':
+            locate = 'olimp'
+        elif f == '2':
+            locate = 'summarinn'
+        elif f == '4':
+            locate = 'aurum'
+
+
     finally:
-        return toner_lvl, prints_count
+        return ip_address, mac_address, host_name, 'KYOCERA', model, locale, toner_lvl, prints_count,
 
 
 def get_prints_toner_hp(ip_address):
